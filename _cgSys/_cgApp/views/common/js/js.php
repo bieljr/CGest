4e7f37849471ac174dd26cf421a290c0c17319ae
unknown <Pedro@.(none)>
2011-03-28 20:51:32 +0100

    Removes outdated Codeigniter base code

--- _cgSys/_cgApp/views/common/js/js.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="js/lib/prototype.js"></script>
-<script type="text/javascript" src="js/lib/scriptaculous.js"></script>
-<script type="text/javascript" src="js/lib/effects.js"></script>
-<script type="text/javascript" src="js/lib/dragdrop.js"></script>
-<script type="text/javascript" src="js/cgest.js"></script>
