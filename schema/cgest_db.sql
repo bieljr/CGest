e62355cfae4ba6a9774ffa1e4caae8eef5afdfb2
Pedro Correia <pedro.a.correia@co.sapo.pt>
2011-05-06 11:38:32 +0100

    Adds initial database schema

---
+++ schema/cgest_db.sql
@@ -0,0 +1,68 @@
+﻿
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES latin1 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+#
+# Structure for the `accounts` table : 
+#
+
+CREATE TABLE `accounts` (
+  `account_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`account_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `banks` table : 
+#
+
+CREATE TABLE `banks` (
+  `bank_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`bank_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `documents` table : 
+#
+
+CREATE TABLE `documents` (
+  `document_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`document_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `transactions` table : 
+#
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `users` table : 
+#
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `vendors` table : 
+#
+
+CREATE TABLE `vendors` (
+  `vendor_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`vendor_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
